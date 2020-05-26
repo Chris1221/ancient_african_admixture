9c2cf92a833b12b62f88a958a6beea52e112426d
Christopher Cole <ccole@well.ox.ac.uk>
2020-05-26 21:15:27 +0100

    Get rid of tex

--- r/MASTER.R
+++ r/MASTER.R
@@ -27,7 +27,7 @@ figure_1 = migration_plot(
   sims1 = figure_1_sim_plots()[[1]], 
   sims2 = figure_1_sim_plots()[[2]],
   sims3 = figure_1_sim_plots()[[3]]) %>% 
-    ggsave(file = "~/repos/dirmig/plot/new_mig_plot.pdf", width = 12, height = 6)
+    ggsave(file = "plot/new_mig_plot.pdf", width = 12, height = 6)
 
 # Figure 2: Ne
 source("r/new_ne_plot.R")
@@ -36,7 +36,7 @@ figure_2(
   single_versus_joint_inference(),
   plot_ne_simulations()
 ) %>%
-  ggsave(file = "~/repos/dirmig/plot/new_ne_figure.pdf", height = 8, width = 12)
+  ggsave(file = "plot/new_ne_figure.pdf", height = 8, width = 12)
 
 # All histories from the SGDP subset.
 source("r/new_ne_plot.R")
@@ -44,13 +44,13 @@ make_all_histories_figure(
   all_eur_histories(),
   all_afr_histories()
 ) %>% 
-  ggsave(file="~/repos/dirmig/plot/individual_populations_averaged.pdf")
+  ggsave(file="plot/individual_populations_averaged.pdf")
 
 # Descriptive statistics and plots for IMF in the SGDP.
 source("r/heatmaps.R")
 imf_heatmap() %>% ggsave(file = "plot/mig/integrated_sgdp.pdf", height = 5.18, width = 14.9)
 imf_heatmap("hgdp") %>% ggsave(file = "plot/mig/imf_hgdp.pdf", height = 5, width = 10)
-simple_imf_boxplot() %>% ggsave(file = "~/repos/dirmig/plot/mig/sgdp_averages.pdf", height = 5.61, width = 12.5, units = "in")
+simple_imf_boxplot() %>% ggsave(file = "plot/mig/sgdp_averages.pdf", height = 5.61, width = 12.5, units = "in")
 sgdp_imf_t_test() # T test numbers on line 113 
 sgdp_imf_t_test_table() # TeX for the T test table
 papuan_linear_model()[["hgdp"]]
@@ -84,7 +84,7 @@ length_plot()[["plot"]] %>% ggsave(file = "plot/both_length.pdf", height = 8, wi
 
 # Simulation line plot
 source("r/sim_line_plot.R")
-simulation_line_plot() %>% ggsave(file = "~/repos/dirmig/plot/sim_line_plot.pdf", h = 6, w = 10)
+simulation_line_plot() %>% ggsave(file = "plot/sim_line_plot.pdf", h = 6, w = 10)
 
 source("r/new_dstats_figure.R")
 plot_reich_figure(
@@ -92,4 +92,4 @@ plot_reich_figure(
   ancientheatmap = reich_ancientheatmap(),
   modern_bar_plot = reich_modern_density(),
   archaic_bar_plot = reich_archaic_density()
-) %>% ggsave(file = "~/repos/dirmig/plot/new_dstats.pdf", height = 8, width = 12)
+) %>% ggsave(file = "plot/new_dstats.pdf", height = 8, width = 12)
